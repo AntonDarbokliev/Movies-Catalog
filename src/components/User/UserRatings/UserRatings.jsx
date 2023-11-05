22f21c546f5d8c71a5a915803d480d426d74a6b7
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-11-05 14:21:35 +0200

    finished user ratings page

--- src/components/User/UserRatings/UserRatings.jsx
+++ src/components/User/UserRatings/UserRatings.jsx
@@ -1,17 +1,35 @@
-import likeIcon from '../../../assets/images/like.png'
-import dislikeIcon from '../../../assets/images/dislike.png'
+import likeIcon from "../../../assets/images/like.png";
+import dislikeIcon from "../../../assets/images/dislike.png";
+import "./UserRatings.css";
 
 export const UserRatings = () => {
-    return(
-        <div id="userRatingsDiv">
-        <h1>Anton's Ratings</h1>
-        <ul id="userRatings">
-            <li>
-                <img src="https://m.media-amazon.com/images/M/MV5BNWI3ZmY4NmItMGQ4My00ODJlLWJlNTktYjk2NzRkODU3YTNlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" alt="poster" />
-                <p>Five nights at Freddy's</p>
-                <img src={likeIcon} alt="" />
-            </li>
-        </ul>
-        </div>
-    )
-}
+  return (
+    <div id="userRatingsDiv">
+      <h1 id="header">Anton's Ratings</h1>
+      <ul id="userRatings">
+      <li className="ratedMovie">
+          <img
+            id="movieImage"
+            src="https://m.media-amazon.com/images/M/MV5BNWI3ZmY4NmItMGQ4My00ODJlLWJlNTktYjk2NzRkODU3YTNlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"
+            alt="poster"
+          />
+          <div id="movieRating">
+            <p>Five nights at Freddy's</p>
+            <img id="ratingIcon" src={likeIcon} alt="like/dislike icon" />
+          </div>
+        </li>
+        <li className="ratedMovie">
+          <img
+            id="movieImage"
+            src="https://m.media-amazon.com/images/M/MV5BNWI3ZmY4NmItMGQ4My00ODJlLWJlNTktYjk2NzRkODU3YTNlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"
+            alt="poster"
+          />
+          <div id="movieRating">
+            <p>Five nights at Freddy's</p>
+            <img id="ratingIcon" src={dislikeIcon} alt="like/dislike icon" />
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
