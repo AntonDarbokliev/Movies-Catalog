b16ec0d4e6c08c9ab88d3d7b5fc47616b59e1069
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-11-07 18:11:57 +0200

    Dynamic home loading

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,16 +2,14 @@ import './Home.css'
 import { MovieCard } from '../Shared/MovieCard/MovieCard.jsx'
 import { useEffect, useState } from 'react'
 export const Home = () => {
-    const [message,setMessage] = useState()
+    const [movies,setMovies] = useState([])
 
     useEffect(() => {
-        fetch('http://localhost:3000/')
-            .then(res => res.json())
-            .then(data => setMessage(data))
+            fetch('http://localhost:3000')
+            .then( res => res.json())
+            .then( data => setMovies(data))
             .catch(err => console.log(err))
-    },[])
-
-    console.log(message);
+   },[])
 
     return (
         <>
@@ -19,7 +17,8 @@ export const Home = () => {
         <h1 id='header'>Latest releases</h1>
         </div>
         <div className='homeMovies'>
-            <MovieCard imageUrl='https://m.media-amazon.com/images/M/MV5BNWI3ZmY4NmItMGQ4My00ODJlLWJlNTktYjk2NzRkODU3YTNlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg' 
+            {movies.map( movie => <MovieCard imageUrl={movie.moviePoster} movieId={movie._id}/>)}
+            {/* <MovieCard imageUrl='https://m.media-amazon.com/images/M/MV5BNWI3ZmY4NmItMGQ4My00ODJlLWJlNTktYjk2NzRkODU3YTNlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg' 
             movieId={15}/>
             <MovieCard imageUrl='https://m.media-amazon.com/images/M/MV5BMmJhYjBkMzgtZGIwMC00YTAzLWE4NTQtYzVkNDVmYjIzODI0XkEyXkFqcGdeQXVyODQxMTI4MjM@._V1_FMjpg_UX1000_.jpg' 
             movieId={12}/>
@@ -28,7 +27,7 @@ export const Home = () => {
              <MovieCard imageUrl='https://m.media-amazon.com/images/M/MV5BMmJhYjBkMzgtZGIwMC00YTAzLWE4NTQtYzVkNDVmYjIzODI0XkEyXkFqcGdeQXVyODQxMTI4MjM@._V1_FMjpg_UX1000_.jpg' 
             movieId={12}/>
             <MovieCard imageUrl='https://m.media-amazon.com/images/M/MV5BMmJhYjBkMzgtZGIwMC00YTAzLWE4NTQtYzVkNDVmYjIzODI0XkEyXkFqcGdeQXVyODQxMTI4MjM@._V1_FMjpg_UX1000_.jpg' 
-            movieId={12}/>
+            movieId={12}/> */}
                          
         </div>
         </>
--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,7 +1,15 @@
+const movieService = require("../services/movieService.js")
+
 const homeController = require("express").Router()
 
-    homeController.get('/', (req,res) => {
-        res.status(200).json({message : 'Test'})
+    homeController.get('/', async (req,res) => {
+        try {
+            console.log('a');
+            const movies = await movieService.getAll()
+            res.status(200).json(movies)
+        } catch (error) {
+            console.log(error);
+        }
     })
 
     module.exports = homeController
