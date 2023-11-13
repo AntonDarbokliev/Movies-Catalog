6d83aeae2da3e4a5d3a68a57df2e4663402bcdc2
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-11-13 19:28:20 +0200

    dynamic navigation

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,17 +2,27 @@ import { Link } from 'react-router-dom'
 import './NavBar.css'
 import logo from '../../assets/images/logo.png'
 import { Dropdown } from '../User/Dropdown/DropDown.jsx'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext.js'
 
 export const NavBar = () => {
+    const {isAuthenticated} = useContext(AuthContext)
+
     return (
         <div className="navBar">
             <img src={logo} alt="logo" className='logo'/>
             <Link to="/" className='link'>Home</Link>
             <Link to="/movie/catalog" className='link'>Catalog</Link>
             <Link to="/about" className='link'>About</Link>
-            <Link to="/user/login" className='link'>Login</Link>
-            <Link to="/user/register" className='link'>Register</Link>
+            {isAuthenticated && 
             <Dropdown/>
+            }
+            {!isAuthenticated &&
+            <>
+            <Link to="/user/register" className='link'>Register</Link>
+            <Link to="/user/login" className='link'>Login</Link>
+            </>
+            }
             
         </div>
     )
