2739a29c39e6d5ec0d880ee1847530db802cb55c
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-11-13 19:02:09 +0200

    auth context and service

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,85 @@
-import { NavBar } from './components/NavBar/NavBar.jsx'
-import { Footer } from './components/Footer/Footer.jsx'
-import { Home } from './components/Home/Home.jsx'
-import { Routes,Route, redirect } from 'react-router-dom'
-import { MovieDetails } from './components/MovieDetails/MovieDetails.jsx'
-import { UserRatings } from './components/User/UserRatings/UserRatings.jsx'
-import { Register } from './components/Auth/Register/Register.jsx'
-import { Login } from './components/Auth/Login/Login.jsx'
-import { Catalog } from './components/Catalog/Catalog.jsx'
-import { About } from './components/About/About.jsx'
+import { NavBar } from "./components/NavBar/NavBar.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import { Home } from "./components/Home/Home.jsx";
+import { Routes, Route, redirect, useNavigate } from "react-router-dom";
+import { MovieDetails } from "./components/MovieDetails/MovieDetails.jsx";
+import { UserRatings } from "./components/User/UserRatings/UserRatings.jsx";
+import { Register } from "./components/Auth/Register/Register.jsx";
+import { Login } from "./components/Auth/Login/Login.jsx";
+import { Catalog } from "./components/Catalog/Catalog.jsx";
+import { About } from "./components/About/About.jsx";
+import { authServiceFactory } from "./services/authService.js";
+import { useState } from "react";
+import { AuthContext } from "./contexts/AuthContext.js";
 
 function App() {
+  const [auth, setAuth] = useState({});
+  const authService = authServiceFactory();
+
+  const navigate = useNavigate();
+
+  const onRegisterSubmit = async (data) => {
+    const { repeatPassword, ...registerData } = data;
+    if (repeatPassword !== registerData.password) {
+      throw new Error("Passwords not matching");
+    }
+
+    try {
+      const result = await authService.register(registerData);
+
+      setAuth(result);
+
+      navigate("/");
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+  const onLoginSubmit = async (data) => {
+    try {
+      const result = await authService.login(data);
+      setAuth(result);
+      navigate("/");
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+  const contextValues = {
+    onRegisterSubmit,
+    onLoginSubmit,
+    userId: auth._id,
+    token: auth.token,
+    isAuthenticated: !!auth.token,
+    username: auth.username,
+  };
 
   return (
     <>
-    <NavBar/>
-    <div className='main'>
-      <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='movie/:movieId/details' element={<MovieDetails/>}></Route>
-      <Route path='user/:id/ratings' element={<UserRatings/>}></Route>
-      <Route path='user/register' element={<Register/>} loader={async () => redirect('/')} ></Route>
-      <Route path='user/login' element={<Login/>}></Route>
-      <Route path='movie/catalog' element={<Catalog/>}></Route>
-      <Route path='*' element={<h1 style={{color : 'white'}}>Page not found (404)</h1>}></Route>
-      </Routes>
-    </div>
-    <Footer/>
+      <AuthContext.Provider value={contextValues}>
+        <NavBar />
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route
+              path="movie/:movieId/details"
+              element={<MovieDetails />}
+            ></Route>
+            <Route path="user/:id/ratings" element={<UserRatings />}></Route>
+            <Route path="user/register" element={<Register />}></Route>
+            <Route path="user/login" element={<Login />}></Route>
+            <Route path="movie/catalog" element={<Catalog />}></Route>
+            <Route
+              path="*"
+              element={<h1 style={{ color: "white" }}>Page not found (404)</h1>}
+            ></Route>
+          </Routes> 
+        </div>
+        <Footer />
+      </AuthContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/services/authService.js
@@ -0,0 +1,13 @@
+import { requestFactory } from "./requester.js";
+
+const baseUrl = 'http://localhost:3000/user'
+
+export const authServiceFactory = () => {
+    const request = requestFactory()    //TODO: Add token parameter to requestFactory
+
+    return {
+        login : (data) => request.post(`${baseUrl}/login`,data),
+        register : (data) => request.post(`${baseUrl}/register`,data),
+        //TODO: Add logout
+    }
+}
