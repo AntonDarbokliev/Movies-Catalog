746dbf44cad2fd9898617f9ebc34c880e32069ad
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-11-20 21:57:13 +0200

    useForm adjustment

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useForm = ( initialValue, onSubmitHandler ) => {
     const [formValues, setFormValues] = useState(initialValue)
+
+    useEffect(() => {
+        if(JSON.stringify(formValues) !== JSON.stringify(initialValue)){
+            setFormValues(initialValue)
+        }
+    },[initialValue])
 
     const onChangeHandler = (e) => {
         setFormValues(state => ({...state,[e.target.name]:e.target.value}))
