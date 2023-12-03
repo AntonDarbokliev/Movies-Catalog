43525c1997022e914b54f7db1229d15649d0a2e4
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-03 17:27:11 +0200

    cors allowedOrigins

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,5 +1,12 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','https://movies-catalog-8mqp.onrender.com');
+
+    const allowedOrigins = ['http://localhost:5173', 'https://movies-catalog-8mqp.onrender.com']
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Auth');
     // res.setHeader('access-control-expose-headers', 'Set-Cookie');
