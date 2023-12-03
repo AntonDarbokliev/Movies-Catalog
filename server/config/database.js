dac7f8cbd54108969553d96b4dee3ca85b85d958
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-03 12:11:35 +0200

    Connect to external atlas db

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-const connectionString = process.env.DATABASE_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/Movie-Catalog'
+const connectionString = process.env.DATABASE_CONNECTION_STRING ||  'mongodb://127.0.0.1:27017/Movie-Catalog'
 
 module.exports = async () => {
     try{
