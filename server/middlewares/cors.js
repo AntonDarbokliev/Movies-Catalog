216863a244eedb67c7347a4c86da1c80f758c25a
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-03 16:08:21 +0200

    cors fix

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:5173', 'https://movies-catalog-8mqp.onrender.com/']);
+    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:5173', 'https://movies-catalog-8mqp.onrender.com']);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Auth');
     // res.setHeader('access-control-expose-headers', 'Set-Cookie');
